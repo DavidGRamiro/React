c78bb44123b0b685ef77cb4e635f1ba1e88c7c36
David González Ramiro <davidgramiro@gmail.com>
2024-03-29 00:39:33 +0100

    Hooks. Seccion 4 Terminada

---
+++ 03-counter-vite/src/CounterApp.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react' 
+import PropTypes from 'prop-types'
+
+export const CounterApp = ({ value }) => {
+
+    // UseState -> Para cambiar el estado del compoennte, en este caso el valor.
+    //  Se establece un valor, al que inicializamos el counter, en este caso 0.
+    // Si queremos cambiarlo, tenedremos que declarar otra variable, seguido de set + el nombre de la variable inicial.
+    // No es obligatorio, es un estandar.
+    const [ counter, setCounter ] = useState(value)
+    
+    // Añadimos un numero al contador.
+    const addCounter = () =>  { 
+        setCounter(counter + 1)
+        // setCounter((c) => c + 1) Esta es otra forma de hacerlo.
+    } 
+
+    // Restamos un valor al contador.
+    const deleteCounter = () => {
+        setCounter( counter -1 )
+    }
+
+    // Reseteamos el contandor al valor por defecto que llega del padre.
+    const resetCounter = () => {
+        setCounter(value)
+    }
+
+    return (
+        <>
+            <h1>Counter App</h1>
+            <h2> { counter } </h2>
+
+
+            {/* No es necesario llamar a la funcion y pasarle el event, viene implicito */}
+            <button onClick={ addCounter }> +1 </button>
+            <button onClick={ deleteCounter }>  - 1 </button>
+            <button onClick={ resetCounter }> Reset </button>
+
+        </>
+    )
+}
+
+
+CounterApp.propTypes = {
+    value: PropTypes.number.isRequired
+}
--- 03-counter-vite/src/main.jsx
+++ 03-counter-vite/src/main.jsx
@@ -1,13 +1,12 @@
+import { CounterApp } from './CounterApp';
 
 import React from 'react';
 import ReactDOM  from 'react-dom/client';
-import { HelloWorld } from './HelloWorld';
-import { FirstApp } from './FirstApp';
 
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp></FirstApp>
+        <CounterApp value={1234}></CounterApp>
     </React.StrictMode>
 )
