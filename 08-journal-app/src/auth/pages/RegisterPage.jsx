814c15363b0ace17c14cb3745be9d17b1428af31
David González Ramiro <davidgramiro@gmail.com>
2024-05-29 17:24:31 +0200

    Layout comun para registro y login

--- 08-journal-app/src/auth/pages/RegisterPage.jsx
+++ 08-journal-app/src/auth/pages/RegisterPage.jsx
@@ -1,9 +1,44 @@
+import { Button, Grid, Link, TextField, Typography } from '@mui/material'
+import { Google } from '@mui/icons-material'
+import { Link as RouterLink } from 'react-router-dom'
+import { AuthLayout } from '../layout/AuthLayout'
 
 export const RegisterPage = () => {
   return (
-    <div>
-      <h1>Registro PAGE</h1>
-    </div>
+    <AuthLayout title='Crear cuenta'>
+
+      <form>
+        <Grid container>
+          {/* Item para el nombre completo */}
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <TextField label="Nombre" type='text' placeholder='Nombre y apellidos' fullWidth></TextField>
+          </Grid>
+          {/* Item para el correo electronico */}
+          <Grid item xs={12} sx={{ mb: 2 }}>
+            <TextField label="Correo electrónico" type='email' placeholder='correo@google.com' fullWidth></TextField>
+          </Grid>
+          {/* Item para la contraseña */}
+          <Grid item xs={12}>
+            <TextField label="Password" type='password' placeholder='Contraseña' fullWidth></TextField>
+          </Grid>
+          {/* Item para boton de crear nueva cuenta del formulario */}
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} >
+              <Button variant='contained' fullWidth>Crear cuenta</Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{ mr: 1 }}> ¿ Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} color='inherit' to="/auth/login"> Iniciar sesión</Link>
+          </Grid>
+
+        </Grid>
+      </form>
+    </AuthLayout>
+
+
+
   )
 }
 
