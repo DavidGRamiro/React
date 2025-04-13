347814157d5aeb0769fd9c7231126e834a90eee3
David Gonzalez <davidgramiro@gmail.com>
2025-04-13 21:17:18 +0200

    UseReducer, inserccion y eliminacion de nuestra lista de todos, presistencia en locaStorage

--- 05-custom-hooks/src/08-useReducer/TodoApp.jsx
+++ 05-custom-hooks/src/08-useReducer/TodoApp.jsx
@@ -1,28 +1,54 @@
-import { useReducer } from "react"
+import { useEffect, useReducer } from "react"
 import { todoReducer } from "./TodoReducer"
 import { TodoList } from "./components/TodoList"
 import { TodoAdd } from "./components/TodoAdd"
 
+
+const initialState= [
+  // {
+  //   id: new Date().getTime(),
+  //   description: 'Aprender React',
+  //   done: false
+  // },
+  
+]
+
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+
 export const TodoApp = () => {
 
-  const initialState= [
-    {
-      id: new Date().getTime(),
-      description: 'Aprender React',
-      done: false
-    },
-    {
-      id: new Date().getTime() + 1000,
-      description: 'Aprender Redux',
-      done: false
-    }
-  ]
 
   // A un useReducer, se le manda el reducer que hemos creado, y un estado incial, en este caso, una lista de TODOS, y el reducer.
-  const [ todos, dispatch] = useReducer( todoReducer, initialState)
+  // Init, es el tercer parametro, que es como incializar el estado del reducer. En este caso, estamos tomando los estados del localStorage
+  const [ todos, dispatch] = useReducer( todoReducer, initialState, init)
+  
+  // Tenemos la necesidad, de que cuando nuestra lista de Todos createImageBitmap, disparar un effecto,
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
 
+  }, [todos])
+  
   const handdleTodoAdd = (value) => {
-    console.log('Componente principal',value)
+    const action = {
+      type: '[TODO] add Todo',
+      payload: value
+    }
+    // Llamamos al reducer con el Dispatch. Esto llamara a nuestro todoReducer,y esto
+    // evaluara en el switch el tipo de action que le hemos pasado.
+    // Retornara una nueva lista de Todos.
+    dispatch( action )
+  }
+
+  const handdledDeleteTodo = (id) => {
+    
+    const action = {
+      type: '[TODO] remove Todo',
+      payload: id
+    }
+    dispatch( action )
   }
 
 
@@ -33,7 +59,7 @@ export const TodoApp = () => {
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos}  /> 
+          <TodoList todos={todos}  onDeleteTodo={handdledDeleteTodo} /> 
         </div>
 
         <div className="col-5">
--- 05-custom-hooks/src/08-useReducer/TodoReducer.js
+++ 05-custom-hooks/src/08-useReducer/TodoReducer.js
@@ -4,8 +4,11 @@ export const todoReducer = ( initialState = [], action) => {
 
 
   switch (action.type) {
-    case ' ABC':
-      throw new Error('Action.type === ABC no esta implementada.');    
+    case '[TODO] add Todo':
+      return [ ...initialState, action.payload ]
+    case '[TODO] remove Todo':
+      // Return un nuevo array, solo aquellos que no contengan el id que le mandamos en el payload.
+      return initialState.filter( todo => todo.id !== action.payload )
     default:
       return initialState;
   }
