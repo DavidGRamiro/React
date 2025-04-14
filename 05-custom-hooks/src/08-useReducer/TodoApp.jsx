1cd26a4116b1974bb2cd215a9f82b60d2ee49136
David Gonzalez <davidgramiro@gmail.com>
2025-04-14 13:31:50 +0200

    Finalizacion de seccion useReducer con TodoApp

--- 05-custom-hooks/src/08-useReducer/TodoApp.jsx
+++ 05-custom-hooks/src/08-useReducer/TodoApp.jsx
@@ -1,71 +1,31 @@
-import { useEffect, useReducer } from "react"
-import { todoReducer } from "./TodoReducer"
+
 import { TodoList } from "./components/TodoList"
 import { TodoAdd } from "./components/TodoAdd"
-
-
-const initialState= [
-  // {
-  //   id: new Date().getTime(),
-  //   description: 'Aprender React',
-  //   done: false
-  // },
-  
-]
-
-const init = () => {
-  return JSON.parse(localStorage.getItem('todos')) || []
-}
+import { useTodo } from "./hooks/useTodo"
 
 
 export const TodoApp = () => {
-
-
-  // A un useReducer, se le manda el reducer que hemos creado, y un estado incial, en este caso, una lista de TODOS, y el reducer.
-  // Init, es el tercer parametro, que es como incializar el estado del reducer. En este caso, estamos tomando los estados del localStorage
-  const [ todos, dispatch] = useReducer( todoReducer, initialState, init)
   
-  // Tenemos la necesidad, de que cuando nuestra lista de Todos createImageBitmap, disparar un effecto,
-  useEffect(() => {
-    localStorage.setItem('todos', JSON.stringify(todos))
-
-  }, [todos])
+  // Tarea FINAL:
+  // Previamente, todo lo que encontramos en nuestro customHook, useTodo, estaba aqui. Creamos un customHook para que sea fácil de leer nuestro componente.
+  const { todos, handdleTodoAdd, handdledDeleteTodo, handdleToggleTodo, penddingTodosCount, todosCount } = useTodo()
   
-  const handdleTodoAdd = (value) => {
-    const action = {
-      type: '[TODO] add Todo',
-      payload: value
-    }
-    // Llamamos al reducer con el Dispatch. Esto llamara a nuestro todoReducer,y esto
-    // evaluara en el switch el tipo de action que le hemos pasado.
-    // Retornara una nueva lista de Todos.
-    dispatch( action )
-  }
-
-  const handdledDeleteTodo = (id) => {
-    
-    const action = {
-      type: '[TODO] remove Todo',
-      payload: id
-    }
-    dispatch( action )
-  }
 
 
   return (
     <>
-      <h1>TodoApp: 10,  <small>pendientes: 2</small></h1>
+      <h1>TodoApp: {todosCount} ,  <small>pendientes: {penddingTodosCount}</small></h1>
       <hr />
 
       <div className="row">
         <div className="col-7">
-          <TodoList todos={todos}  onDeleteTodo={handdledDeleteTodo} /> 
+          <TodoList todos={todos}  onDeleteTodo={handdledDeleteTodo} onToggleTodo={handdleToggleTodo} /> 
         </div>
 
         <div className="col-5">
           <h4>Agregar TODO.</h4>
           <hr />
-          <TodoAdd  emitTodoAdd={ (value) => handdleTodoAdd(value)}/>
+          <TodoAdd  emitTodoAdd={ handdleTodoAdd} />
         </div>
       </div>
     </>
---
+++ 05-custom-hooks/src/08-useReducer/hooks/useTodo.js
@@ -0,0 +1,60 @@
+import { useEffect, useReducer } from "react"
+import { todoReducer } from "../TodoReducer"
+
+
+const initialState = []
+const init = () => {
+  return JSON.parse(localStorage.getItem('todos')) || []
+}
+
+export const useTodo = ()  => {
+  
+
+  // A un useReducer, se le manda el reducer que hemos creado, y un estado incial, en este caso, una lista de TODOS, y el reducer.
+  // Init, es el tercer parametro, que es como incializar el estado del reducer. En este caso, estamos tomando los estados del localStorage
+  const [ todos, dispatch] = useReducer( todoReducer, initialState, init)
+
+  
+  // Tenemos la necesidad, de que cuando nuestra lista de Todos createImageBitmap, disparar un effecto,
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+
+  }, [todos])
+  
+  const handdleTodoAdd = (value) => {
+    const action = {
+      type: '[TODO] add Todo',
+      payload: value
+    }
+    // Llamamos al reducer con el Dispatch. Esto llamara a nuestro todoReducer,y esto
+    // evaluara en el switch el tipo de action que le hemos pasado.
+    // Retornara una nueva lista de Todos.
+    dispatch( action )
+  }
+
+  const handdledDeleteTodo = (id) => {
+    const action = {
+      type: '[TODO] remove Todo',
+      payload: id
+    }
+    dispatch( action )
+  }
+
+  const handdleToggleTodo = (id) => {
+    const action = {
+      type: '[TODO] toggle Todo',
+      payload: id
+    }
+    dispatch( action )
+  }
+
+  return {
+    todos,
+    handdleTodoAdd,
+    handdledDeleteTodo,
+    handdleToggleTodo,
+    todosCount: todos.length, 
+    penddingTodosCount : todos.filter( todo => !todo.done).length
+  }
+}
+
